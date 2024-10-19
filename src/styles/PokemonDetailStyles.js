214a2267628fba2a18b245164fc48dff7d9221a5
Gema <tgemamatt@gmail.com>
2024-10-19 15:53:33 -0500

    fix styles

--- src/styles/PokemonDetailStyles.js
+++ src/styles/PokemonDetailStyles.js
@@ -1,6 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
   card: { 
     padding: 10,
     margin: 10,
